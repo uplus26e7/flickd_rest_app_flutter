154367b8a0897a8f448b8a0000ca87aa0fa549f7
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2021-06-28 19:54:16 +0900

    💄 Add Splash Page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+  const SplashPage({Key? key, required this.onInitializationComplete})
+      : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flickd',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage("assets/images/logo.png"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
